b0b1179e57654465517f115703e08cc6f3965f0c
Anastasia Polozova <polozova.anastasi@gmail.com>
2019-11-29 16:49:42 +0200

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://i.pinimg.com/originals/06/52/6a/06526a7805cba3ec66b0b6b4aea63c69.jpg" alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://image.shutterstock.com/image-photo/panorama-foggy-forest-fairy-tale-260nw-1123858610.jpg" alt=""/>
+        </div>
+        <div>
+          ava+desc
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCE9XcZRF7unwI896MbBpoo32fp21yZ-4RIApDGdSZ6e09Y_GK" alt=""/>
+        </div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+        
+        
+      </div>
+    );
+}
+
+export default Profile;
